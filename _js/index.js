a96faa8277f3a581cbfd778bacf325c6d0a8ce4f
stresslimit <colin@slowfactory.com>
2023-09-13 12:52:07 -0400

    NEW SITE DESIGN

--- _js/index.js
+++ _js/index.js
@@ -1,10 +1,35 @@
+import Splide from '@splidejs/splide'; // https://splidejs.com/  https://github.com/Splidejs/splide
 
 window.addEventListener('DOMContentLoaded', (e) => {
 
   externalLinks()
+  homeRollovers()
+
+  // slider banner
+  if (document.querySelector('.splide')) {
+    const collection = document.getElementsByClassName('splide');
+    for (let item of collection) {
+      if (item.id) {
+        new Splide(`#${item.id}`).mount();
+      }
+    }
+  }
 
 })
 
+
+function homeRollovers() {
+  let a = document.querySelectorAll('a.roller')
+  a.forEach((el) => {
+    el.addEventListener('mouseover', function() {
+      // console.log(`rolled-${el.dataset.for}`)
+      document.getElementById(`rolled-${el.dataset.for}`).style.display = 'block'
+    })
+    el.addEventListener('mouseout', function() {
+      document.getElementById(`rolled-${el.dataset.for}`).style.display = 'none'
+    })
+  })
+}
 
 
 function externalLinks() {
